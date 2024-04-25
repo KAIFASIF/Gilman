76d70b55be134d728df572b3523b9c7e095884b9
kaif <asifkaif21@gmail.com>
2024-04-25 22:29:43 +0530

    asdas

--- src/services/bookingApiService.ts
+++ src/services/bookingApiService.ts
@@ -14,11 +14,11 @@ export const getBookings = (id: number) => {
 };
 
 export const addSlot = () => {
-  return api.get(`/addScheduleBookings`);
+  return api.get(`http://3.36.54.14:9000/api/v1/user/addScheduleBookings`);
 };
 
 export const deleteSlot = () => {
-  return api.get(`/reset`);
+  return api.get(`http://3.36.54.14:9000/api/v1/user/reset`);
 };
 
 export const fetchSlots = (date: string) => {
