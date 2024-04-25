4f7a42ecb90521479446a98f4aa871535ba7f214
KAIFASIF <89296630+KAIFASIF@users.noreply.github.com>
2024-04-25 21:39:23 +0530

    Update userApiService.ts

--- src/services/userApiService.ts
+++ src/services/userApiService.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const signupUser = (data: any) => {
-  return axios.post(`http://ec2-43-202-42-35.ap-northeast-2.compute.amazonaws.com:9000/api/v1/user/signup`, data);
+  return axios.post(`http://3.36.54.14:9000/api/v1/user/signup`, data);
   // return axios.post(`http://localhost:9000/api/v1/user/signup`, data);
 };
 
 export const signinUser = (data: any) => {
-  return axios.post(`http://ec2-43-202-42-35.ap-northeast-2.compute.amazonaws.com:9000/api/v1/user/signin`, data);
+  return axios.post(`http://3.36.54.14:9000/api/v1/user/signin`, data);
   // return axios.post(`http://localhost:9000/api/v1/user/signin`, data);
 };
 
